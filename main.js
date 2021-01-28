3155c8e7c88aa90d759e9d7e6de74dd094db870f
sergey_naydenko <lancers0492@gmail.com>
2021-01-28 20:35:58 +0200

    emulate files loading

--- main.js
+++ main.js
@@ -1,43 +1,131 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
-const path = require('path')
+const { app, BrowserWindow, screen } = require("electron");
 
-function createWindow () {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+// good Promise library
+const bluebird = require("bluebird");
+const path = require("path");
+
+const pLimit = require("p-limit");
+
+const rnd = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+
+function createProgressWindow({ x, y, title }) {
+  const win = new BrowserWindow({
+    x,
+    y,
+    width: 280,
+    height: 70,
+    title,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
-  })
+      contextIsolation: false,
+      devTools: true,
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
 
-  // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  win.loadFile("index.html");
 
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  return win;
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.whenReady().then(() => {
-  createWindow()
-  
-  app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-})
+async function downloadFile({ speed = 10, fileName }) {
+  const { width, height } = screen.getPrimaryDisplay().workAreaSize;
+  const win = createProgressWindow({
+    x: rnd(200, width - 450),
+    y: rnd(150, height - 300),
+    title: fileName, 
+  });
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
-})
+  console.log("starting loading", fileName);
+  console.time(fileName);
+  for (let i = 0; i <= 100; i += 2) {
+    win.webContents.send("progress", i);
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+    win.setProgressBar(i / 100);
+
+    await bluebird.delay(speed);
+  }
+  console.timeEnd(fileName);
+
+  win.close();
+}
+
+async function queue(limit, data, fn, cb = () => {}) {
+  const promises = [];
+  let counter = 0;
+  let ready = 0;
+
+  const next = () =>
+    (counter < data.length) &&
+    fn(data[counter++]).then(() => cb(++ready)).then(next);
+
+  for (let i = 0; i < limit; i++) {
+    promises.push(next());
+  }
+
+  await bluebird.all(promises);
+}
+
+async function v1(limit, data, fn) {
+  let i = 0;
+  let loading = 0;
+
+  while (i < data.length) {
+    const promises = [];
+
+    while (loading < limit) {
+      promises.push(fn(data[i]).then(() => loading--));
+      i += 1;
+      loading++;
+      if (i >= data.length) break;
+    }
+
+    await Promise.all(promises);
+  }
+}
+
+(async () => {
+  await app.whenReady();
+  app.on("window-all-closed", () => { /* app.quit() */ });
+
+  const data = Array(16).fill().map((v, i) => ({
+    speed: rnd(10, 30),
+    fileName: `File ${i + 1}`,
+  }));
+
+  console.log('trying custom v1 function');
+  process.stdout.write('\r3')
+  await bluebird.delay(1000);
+  process.stdout.write('\r2')
+  await bluebird.delay(1000);
+  process.stdout.write('\r1')
+  await bluebird.delay(1000);
+  await v1(5, data, downloadFile);
+
+  console.log('trying another way: p-limit module');
+  process.stdout.write('\r3')
+  await bluebird.delay(1000);
+  process.stdout.write('\r2')
+  await bluebird.delay(1000);
+  process.stdout.write('\r1')
+  await bluebird.delay(1000);
+
+  // p-limit module
+  const limit = pLimit(5);
+  await Promise.all(data.map(item => limit(() => downloadFile(item))));
+
+  console.log('trying custom way 2: queue function');
+  process.stdout.write('\r3')
+  await bluebird.delay(1000);
+  process.stdout.write('\r2')
+  await bluebird.delay(1000);
+  process.stdout.write('\r1')
+  await bluebird.delay(1000);
+
+  await queue(5, data, downloadFile, (i) => {
+    console.log("queue progressing:", i, "of total", data.length);
+  });
+
+  // exit main app
+  app.quit();
+})().catch(console.error);
--- preload.js
+++ preload.js
@@ -1,12 +1,26 @@
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
+
+const { ipcRenderer } = require('electron')
+
+let progressElem;
+let labelElem;
+
+ipcRenderer.on('progress', (event, arg) => {
+  progressElem && (progressElem.value = arg);
+  labelElem && (labelElem.innerText = arg);
+});
+
 window.addEventListener('DOMContentLoaded', () => {
+  progressElem = document.querySelector('#progress');
+  labelElem = document.querySelector('label');
+  
   const replaceText = (selector, text) => {
     const element = document.getElementById(selector)
-    if (element) element.innerText = text
+    if (element) element.innerText = text;
   }
 
   for (const type of ['chrome', 'node', 'electron']) {
-    replaceText(`${type}-version`, process.versions[type])
+    replaceText(`${type}-version`, process.versions[type]);
   }
 })
